4dd44358c6872605283188bf8fba45abd4de60d5
jgromerou <jgromerouro@gmail.com>
2023-11-07 19:03:31 -0300

    se borraron la carpeta img del src y las imagenes de la carpeta build que eran jpg. se comentó las funciones que permiten reducir el tamaño de las imagenes.

--- gulpfile.js
+++ gulpfile.js
@@ -21,20 +21,20 @@ function css(done) {
   done();
 }
 
-function imagenes() {
-  return src('src/img/**/*')
-    .pipe(imagemin({ optimizationLevel: 3 }))
-    .pipe(dest('build/img'));
-}
+// function imagenes() {
+//   return src('src/img/**/*')
+//     .pipe(imagemin({ optimizationLevel: 3 }))
+//     .pipe(dest('build/img'));
+// }
 
-function versionWebp() {
-  const opciones = {
-    quality: 50,
-  };
-  return src('src/img/**/*.{png,jpg,jpeg}')
-    .pipe(webp(opciones))
-    .pipe(dest('build/img'));
-}
+// function versionWebp() {
+//   const opciones = {
+//     quality: 50,
+//   };
+//   return src('src/img/**/*.{png,jpg,jpeg}')
+//     .pipe(webp(opciones))
+//     .pipe(dest('build/img'));
+// }
 
 // function versionAvif() {
 //   const opciones = {
@@ -47,15 +47,15 @@ function versionWebp() {
 
 function dev() {
   watch('src/scss/**/*.scss', css);
-  watch('src/img/**/*', imagenes);
+  //watch('src/img/**/*', imagenes);
 }
 
 exports.css = css;
 exports.dev = dev;
 //exports.imagenes = imagenes;
-exports.versionWebp = versionWebp;
+//exports.versionWebp = versionWebp;
 
-exports.default = series(imagenes, versionWebp, css, dev);
+exports.default = series(css, dev);
 
 //series - se inicia una tarea, y hasta que finaliza, inicia la siguiente tarea
 
